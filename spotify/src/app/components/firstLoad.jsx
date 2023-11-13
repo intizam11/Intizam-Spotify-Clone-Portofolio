ec3ca23ea94d9133c789258a702da51645ec0aee
intizam <weirdfunny.official@gmail.com>
2023-11-13 22:34:20 +0700

    news

--- spotify/src/app/components/firstLoad.jsx
+++ spotify/src/app/components/firstLoad.jsx
@@ -1,11 +1,9 @@
-import Image from 'next/image';
-
-
+import Image from "next/image";
 
 export default function Openning() {
   return (
     <>
-      <div className=" h-screen flex items-center">   
+      <div className="h-screen flex items-center bg-black">
         <div className=" mx-auto ">
           <Image
             className="w-36 h-36 animate__animated  animate__bounce "
@@ -15,7 +13,6 @@ export default function Openning() {
             height={144}
           />
           <h1 className="font-medium text-gray-500">Intizam | Fullstack</h1>
-        
         </div>
       </div>
     </>
